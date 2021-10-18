2ca05b28fa2be5d44ff63ea30caf9d40c7782299
Steven <steven@Stevens-Air.home>
2021-10-18 01:44:36 -0400

    scraper

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import pandas as pd
+from datetime import datetime
 import time
 
 
@@ -9,7 +10,7 @@ def scrape_odds(url, xpaths):
         URL where odds are (str)
         xpaths to teams and odds (list)
     Returns:
-        Teams (list), odds (list)
+        Dictionary - keys: teams (list), values: odds (list)
     """
     with webdriver.Firefox() as driver:
         driver.get(url)
@@ -25,14 +26,11 @@ def scrape_odds(url, xpaths):
         
         driver.close()
         
-    return teams, mlines
-
-
-def get_strtime():
-    """Return a string of GMT time"""
-    gmt = time.gmtime()[:6]
-    strtime = f"{gmt[0]}{gmt[1]}{gmt[2]}_{gmt[3]}:{gmt[4]}:{gmt[5]}"
-    return strtime
+    mline_df = pd.DataFrame.from_dict(dict(team=teams, moneyline=mlines))
+    mline_df['date'] = datetime.today().strftime("%m-%d-%y")
+    print(mline_df)
+    
+    return mline_df
 
 
 def save_odds(url, xpaths, bookie, subdir="odds"):
@@ -45,17 +43,27 @@ def save_odds(url, xpaths, bookie, subdir="odds"):
     Returns:
         None
     """
-    teams, mlines = scrape_odds(url, xpaths)
-    odds_df = pd.DataFrame.from_dict(dict(team=teams, moneyline=mlines))
-    print(odds_df)
-    odds_df.to_csv(f"{subdir}/{bookie}_" + get_strtime())
+    mline_df = scrape_odds(url, xpaths)
+    
+    try:
+        old_df = pd.read_csv(f"{subdir}/{bookie}", index_col=0)
+        pd.concat([old_df, mline_df]).to_csv(f"{subdir}/{bookie}")
+    except:
+        mline_df.to_csv(f"{subdir}/{bookie}")
     
     
 if __name__ == "__main__":
     
-    #DraftKings
-    url = "https://sportsbook.draftkings.com/leagues/football/88670561?category=game-lines&subcategory=game"
+    bookie = "DraftKings"
+    url = "https://sportsbook.draftkings.com/leagues/football/88670561"
     xpaths = ["//div[@class='event-cell__name-text']",
               "//span[@class='sportsbook-odds american no-margin default-color']"]
     
-    save_odds(url, xpaths, "draftkimgs")
+    save_odds(url, xpaths, bookie)
+    
+    bookie = "BetMGM"
+    url = "https://sports.va.betmgm.com/en/sports/football-11/betting/usa-9"
+    xpaths = ["//div[@class='participant']",
+              "//div[@class='option option-value']"]
+    
+    save_odds(url, xpaths, bookie)
